028140d29e98e1f42adbd86b01c4c69462a30b0d
Tyler Kuhn <tyler@wargoats.com>
2018-12-15 18:23:32 -0800

    added some ecc

--- src/App.vue
+++ src/App.vue
@@ -15,10 +15,45 @@ export default {
   }
 }
 
-const node = new window.Ipfs()
-node.on('ready', () => {
-  console.log("IPFS Ready!", node);
+// IPFS...
+const ipfs = new window.Ipfs()
+ipfs.on('ready', async () => {
+  console.log("IPFS Ready!");
+
+  // Load/Create a private public key pair...
+  const { localStorage } = window;
+  let ecc = eosjs_ecc
+  const privateKey = await ecc.randomKey()
+  console.log("privateKey:", privateKey)
+  const publicKey = ecc.privateToPublic(privateKey)
+  console.log("publicKey:", publicKey)
+
+  // Generate a random number, with proof...
+  const random = Math.random().toString()
+  const signature = ecc.sign(random, privateKey);
+  console.log("Signature:", signature)
+
+  // Verify the signature/proof...
+  const verified = ecc.verify(signature, random, publicKey)
+  console.log("Verified:", verified)
+
+  let game = {
+    publicKey,
+    signature
+  }
+  let content = ipfs.types.Buffer.from(game.toString());
+  let results = await ipfs.files.add(content);
+  let hash = results[0].hash; // "Qm...WW"
+  console.log("Game Saved to address:", hash)
+
+  ipfs.files.get(hash, function (err, files) {
+    files.forEach((file) => {
+      //console.log("Pulled from address:", file.path)
+      //console.log(file.content.toString('utf8'))
+    })
+  })
 })
+
 </script>
 
 <style>
