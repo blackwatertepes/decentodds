6117ae023ee82363307ddfc58f6933b9fd7dbb3c
Tyler Kuhn <tyler@wargoats.com>
2019-02-03 20:50:12 -0800

    move the cards to their own component

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,28 @@
   <div id="app">
     <router-link :to="{ name: 'root' }">Home</router-link>
     <router-view></router-view>
+    <span>{{ random }}</span>
+    <GameTable></GameTable>
   </div>
 </template>
 
 <script>
   import Ipfs from '../node_modules/ipfs/dist'
-  import Deck from 'deck-of-cards'
+  import GameTable from './components/GameTable.vue'
 
   export default {
     name: 'app',
+    components: {
+      GameTable
+    },
     computed: {
       privateKey() { return this.$store.state.privateKey },
-      publicKey() { return this.$store.getters.publicKey }
+      publicKey() { return this.$store.getters.publicKey },
+      random() { return Math.random() * Number.MAX_SAFE_INTEGER ^ ((new Date()).getMilliseconds() / 1000 * Number.MAX_SAFE_INTEGER) }
     },
     mounted: function() {
       console.log("App Mounted!")
-      
+
       this.$store.dispatch('refreshGames')
 
       const ipfs = window.ipfs = new Ipfs()
@@ -25,17 +31,6 @@
         console.log("IPFS Ready!")
         this.$store.commit('setIpfs', { ipfs })
       })
-
-      let $container = document.getElementById('container')
-
-      // create Deck
-      let deck = window.deck = Deck()
-
-      // add to DOM
-      deck.mount($container)
-
-      deck.fan()
-      deck.flip()
     }
   }
 </script>
@@ -47,10 +42,6 @@
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
     color: #2c3e50;
-    margin-top: 350px;
-  }
-  #container {
-    top: 200px;
   }
   button {
     padding: 3px;
---
+++ src/components/GameTable.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="game-table">
+  </div>
+</template>
+
+<script>
+  import Deck from 'deck-of-cards'
+
+  export default {
+    name: 'GameTable',
+    computed: {
+    },
+    mounted: function() {
+      let $container = document.getElementById('game-table')
+
+      // create Deck
+      let deck = window.deck = Deck()
+
+      // add to DOM
+      deck.mount($container)
+
+      deck.cards[0].x = 600;
+      deck.cards[0].y = 400;
+
+      deck.fan()
+      deck.flip()
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  #game-table {
+    position: fixed;
+    top: calc(50% + 1.5em);
+    left: 50%;
+  }
+</style>
