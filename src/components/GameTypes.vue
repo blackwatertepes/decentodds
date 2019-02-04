54cd3d0c0000670220dff35bbaad2c33c52b6e46
Tyler Kuhn <tyler@wargoats.com>
2019-02-03 20:50:12 -0800

    move the cards to their own component

--- src/components/GameTypes.vue
+++ src/components/GameTypes.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="games-types">
-    <span>{{ random }}</span>
     <ul id="game-types-list">
       <li v-for="({ id:gametype, name:game }, idx) in gameTypes" :key="idx">
         <router-link :to="{ name: 'gametype', params: { gametype } }">{{ game }}</router-link>
@@ -14,7 +13,6 @@
     name: 'GameTypes',
     computed: {
       gameTypes() { return this.$store.state.gameTypes },
-      random() { return Math.random() * Number.MAX_SAFE_INTEGER ^ ((new Date()).getMilliseconds() / 1000 * Number.MAX_SAFE_INTEGER) }
     },
   }
 </script>
